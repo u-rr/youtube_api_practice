a5a7e4c2b9b20925e5613978b724acc33ea1ec82
u-rr <fashiondrop6@gmail.com>
2021-04-28 13:44:29 +0900

    mongoDB atlas導入＋GAEにデプロイ

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 import os
 from pymongo import MongoClient
 from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
 from get_collection import count_videos, get_videos_id_top_viewcount, get_videos_id_latest_published
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
-YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
-USERNAME = os.environ["USERNAME"]
-PASSWORD = os.environ["PASSWORD"]
-DB_URL = os.environ["DB_URL"]
+users = {
+    "user": "psuN6KPb",
+}
+
+# USERNAME = os.environ["USERNAME"]
+# PASSWORD = os.environ["PASSWORD"]
 # mongo_client = MongoClient(f"mongodb://{USERNAME}:{PASSWORD}@mongo:27017/")
 # collection = mongo_client.youtube.videos
+
+DB_URL = os.environ["DB_URL"]
+BASIC_USER = os.environ["BASIC_USER"]
+BASIC_PASSWORD = os.environ["BASIC_PASSWORD"]
 mongo_client = MongoClient(DB_URL)
 collection = mongo_client.youtube.videos
 search_words = ["岸優太", "平野紫耀", "永瀬廉", "神宮寺勇太", "髙橋海人", "king&prince"]
 
+users = {
+    BASIC_USER: BASIC_PASSWORD,
+}
+
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
 
 @app.route("/")
+@auth.login_required
 def index(collection=collection, search_words=search_words):
 
     # count_videos = main()
